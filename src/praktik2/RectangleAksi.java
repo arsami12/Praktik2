2d30a5ca3e3fe75e8fecb4f3739d6778d4db7132
kendi ardi <arsamisapper12@gmail.com>
2018-03-30 09:21:20 +0800

    Mengisi Nilai Atribute

--- src/praktik2/RectangleAksi.java
+++ src/praktik2/RectangleAksi.java
@@ -3,5 +3,8 @@ package praktik2;
 public class RectangleAksi {
     public static void main(String[] args) {
         Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar = 3;
     }
+    
 }
